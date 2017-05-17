a625a02ebfb4e12f4fde2afcb63fa4608cac7a4c
Girum Hagos <hagosgirum@gmail.com>
2017-05-16 22:34:36 -0400

    update poster image urls for movie instances
    deleted the forward slashes

--- entertainment_center.py
+++ entertainment_center.py
@@ -8,13 +8,11 @@ toy_story = media.Movie("Toy Story",
 
 avatar = media.Movie("Avatar",
                      "A marine on an alien planet",
-                     "https://upload.wikimedia.org/wikipedia/en/thumb/b/b0/\
-                     Avatar-Teaser-Poster.jpg/220px-Avatar-Teaser-Poster.jpg",
+                     "https://upload.wikimedia.org/wikipedia/en/thumb/b/b0/Avatar-Teaser-Poster.jpg/220px-Avatar-Teaser-Poster.jpg",
                      "https://www.youtube.com/watch?v=5PSNL1qE6VY")
 school_of_rock = media.Movie("School of Rock",
                      "Storyline",
-                     "https://upload.wikimedia.org/wikipedia/en/thumb/1/11/\
-                     School_of_Rock_Poster.jpg/220px-School_of_Rock_Poster.jpg",
+                     "https://upload.wikimedia.org/wikipedia/en/thumb/1/11/School_of_Rock_Poster.jpg/220px-School_of_Rock_Poster.jpg",
                      "https://www.youtube.com/watch?v=XCwy6lW5Ixc")
 ratatouile = media.Movie("Ratatouille",
                      "Storyline",
@@ -22,13 +20,11 @@ ratatouile = media.Movie("Ratatouille",
                      "https://www.youtube.com/watch?v=c3sBBRxDAqk")
 midnight_in_paris = media.Movie("Midnight in Paris",
                      "Storyline",
-                     "https://upload.wikimedia.org/wikipedia/en/\
-                     thumb/9/9f/Midnight_in_Paris_Poster.jpg/220px-Midnight_in_Paris_Poster.jpg",
+                     "https://upload.wikimedia.org/wikipedia/en/thumb/9/9f/Midnight_in_Paris_Poster.jpg/220px-Midnight_in_Paris_Poster.jpg",
                      "https://www.youtube.com/watch?v=FAfR8omt-CY")
 hunger_games = media.Movie("Hunger Games",
                      "Storyline",
-                     "https://upload.wikimedia.org/wikipedia/en/thumb/4/\
-                     42/HungerGamesPoster.jpg/220px-HungerGamesPoster.jpg",
+                     "https://upload.wikimedia.org/wikipedia/en/thumb/4/42/HungerGamesPoster.jpg/220px-HungerGamesPoster.jpg",
                      "https://www.youtube.com/watch?v=mfmrPu43DF8")
 # Calling movie class on instances
 movies = [toy_story, avatar, school_of_rock, ratatouile, midnight_in_paris,hunger_games]
